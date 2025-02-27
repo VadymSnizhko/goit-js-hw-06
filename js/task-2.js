2155ee9d957cd2776ac10925f24726fb4e9c0fbb
VadymSnizhko <snizhko@gmail.com>
2025-02-27 23:02:50 +0200

    second task hw 06

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,6 @@
- class Storage {
+class Storage {
   #items;
+
   constructor(item){
     this.#items = item;
   }
@@ -7,4 +8,23 @@
   getItems(){
     return this.#items;
   }
+
+  addItem(newItem){
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
  }
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
